7ffc341e258320cedec4e00931d47433d6d32da5
Dou Jones <MISTERCHIEFEXECUTIVEOFFICER@YAHOO.COM>
2019-08-25 19:17:51 -0400

    Add files via upload

---
+++ Nike/Nike.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import {Title} from '../../CSS/title'
+
+export const Nike = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr;
+gap: 60px;
+`
+
+export const Nike1 = styled(Title)`
+height: 100px;
+padding: 10px;
+font-size: 20px;
+background-image: ${({img}) => `url(${img});`}
+background-position: center;
+background-size: cover;
+filter: contrast(75%);
+border-radius: 7px;
+margin-top: 5px;
+transition-duration: .1s;
+transition-property: box-shadow margin-top filter;
+box-shadow: 1px 0px 10px 0px orange;
+&:hover{
+  cursor: pointer;
+  filter: contrast(100%);
+  box-shadow: 0px 0px 10px 0px grey;
+  margin-top: 0px;
+  margin-bottom: 5px;
+}
+`
+
+export const NikeOneLabel = styled.div`
+position: absolute;
+background-color: white;
+padding: 5px;
+`
