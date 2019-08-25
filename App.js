962eaa3e7510315a88e306610c497083f70c31cb
Dou Jones <misterchiefexecutiveofficer@yahoo.com>
2019-08-25 19:08:45 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Navbar } from "./Navbar/Navbar"
+import { Banner } from "./banner/Banner"
+import { Menu } from "./Components/Menu/Menu"
+import { GlobalStyle } from "./CSS/GlobalStyle"
+
+
+function App() {
+  return (
+    <>
+      <GlobalStyle/>
+      <Navbar/>
+      <Banner img/>
+     <Menu/>
+    </>
+  );
+}
+
+export default App;
+
---
+++ Components/Menu/Menu.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import {texts} from '../Info/Info'
+import {Nike, Nike1, NikeOneLabel} from "../Nike/Nike"
+
+const Menu2 = styled.div`
+margin: 0px 400px 50px 21px;
+height: 1000px;
+`
+
+export function Menu(){
+  return (
+    <Menu2>
+    {Object.entries(texts).map(([sectionName, texts]) =>(
+    <>
+    <h1>{sectionName}</h1>
+  <Nike>
+  {texts.map(nike1 => (
+   <Nike1 img={nike1.img}>
+   <NikeOneLabel>{nike1.name}</NikeOneLabel>
+   </Nike1>
+  ))}
+  </Nike>
+  </>
+))}
+  </Menu2>
+);
+}
+
